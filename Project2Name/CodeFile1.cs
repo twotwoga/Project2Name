9986903dc59549f001d0e3ef6b38e6f473463974
twotwoga <twogarin@bk.ru>
2021-04-12 11:36:40 +0500

    Добавьте файлы проекта.

---
+++ Project2Name/CodeFile1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualBasic;
+using System.Windows.Forms;
+using System;
+
+class InputDialogDemo
+{
+    static void Main()
+    {
+        string name, res, txt;
+
+        int year = 2021, age, born;
+        
+        name = Interaction.InputBox(
+            "Ваше ФИО",
+            "Давайте познакомимся"
+            );
+
+        res = Interaction.InputBox("В каком году вы родились?", "Год рождения");
+
+        born = Int32.Parse(res);
+
+        age = year - born;
+
+        txt = name + " Тогда вам " + age + " лет ";
+
+
+        //string txt1 = "очень приятно, " + name + "!";
+
+        MessageBox.Show(txt, "Знакомство состоялось");
+    }
+}
